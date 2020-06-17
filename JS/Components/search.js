918f3d58962caa0a86c83db358c4cb07f92cee6c
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2020-06-16 18:46:38 -0600

    added different serach methods

--- JS/Components/search.js
+++ JS/Components/search.js
@@ -21,6 +21,48 @@ class request {
     });
   }
 
+  static searchByCategory(category){
+    //   create promise
+    return new Promise(function (resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      // make call to api
+      xhr.open(
+        "GET",
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`,
+        true
+      );
+
+      // on success
+      xhr.onload = function() {
+        const response = JSON.parse(xhr.responseText);
+        resolve(response);
+      };
+
+      xhr.send();
+  });
+  }
+
+  static searchByArea(area){
+    //   create promise
+    return new Promise(function (resolve, reject) {
+      const xhr = new XMLHttpRequest();
+      // make call to api
+      xhr.open(
+        "GET",
+        `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`,
+        true
+      );
+
+      // on success
+      xhr.onload = function() {
+        const response = JSON.parse(xhr.responseText);
+        resolve(response);
+      };
+
+      xhr.send();
+  });
+  }
+
   static getMealDetails(id){
     return new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
