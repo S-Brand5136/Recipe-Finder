9d69401f70fe071de988ba7524d080c4e7e0e46b
S-Brand5136 <Brandon.Shemilt@live.co.uk>
2021-01-10 19:14:01 -0700

    add breadCrumb for recipePage

--- src/pages/RecipePage.js
+++ src/pages/RecipePage.js
@@ -10,10 +10,12 @@ import IngredientMeasurementList from "../components/recipePageComponents/Ingred
 // Material UI imports
 import {
   Box,
+  Breadcrumbs,
   Divider,
   Grid,
   makeStyles,
   LinearProgress,
+  Link,
   Typography,
 } from "@material-ui/core";
 
@@ -31,6 +33,14 @@ const useStyles = makeStyles((theme) => ({
   imageStyling: {
     height: "auto",
     maxWidth: "auto",
+  },
+  MuiBreadCrumb: {
+    position: "absolute",
+    right: "3.5rem",
+    [theme.breakpoints.down("lg")]: {
+      top: "10rem",
+      right: "6rem",
+    },
   },
 }));
 
@@ -64,35 +74,45 @@ const RecipePage = ({ match }) => {
             at it :(
           </Typography>
         ) : (
-          <Grid
-            container
-            direction="row"
-            spacing={5}
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} lg={6}>
-              <img
-                className={classes.imageStyling}
-                src={mealDetails.strMealThumb}
-              ></img>
-            </Grid>
-            <InfoText
-              title={mealDetails.strMeal}
-              area={mealDetails.strArea}
-              category={mealDetails.strCategory}
-              youtube={mealDetails.strYotube}
-            />
-            <Grid item xs={12} lg={6}>
-              <IngredientMeasurementList
-                ingredientList={ingredientsList}
-                measurementList={measurementsList}
+          <>
+            <Breadcrumbs aria-label="history" className={classes.MuiBreadCrumb}>
+              <Link color="inherit" href="/homepage">
+                Recipe Search
+              </Link>
+              <Typography style={{ color: "black" }}>
+                {mealDetails.strMeal}
+              </Typography>
+            </Breadcrumbs>
+            <Grid
+              container
+              direction="row"
+              spacing={5}
+              justify="center"
+              alignItems="center"
+            >
+              <Grid item xs={12} lg={6}>
+                <img
+                  className={classes.imageStyling}
+                  src={mealDetails.strMealThumb}
+                ></img>
+              </Grid>
+              <InfoText
+                title={mealDetails.strMeal}
+                area={mealDetails.strArea}
+                category={mealDetails.strCategory}
+                youtube={mealDetails.strYotube}
               />
+              <Grid item xs={12} lg={6}>
+                <IngredientMeasurementList
+                  ingredientList={ingredientsList}
+                  measurementList={measurementsList}
+                />
+              </Grid>
+              <Grid item xs={12} lg={6}>
+                <Instructions instructions={mealDetails.strInstructions} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} lg={6}>
-              <Instructions instructions={mealDetails.strInstructions} />
-            </Grid>
-          </Grid>
+          </>
         )}
       </Box>
     </div>
